07e6603d9800e7c560e157fb2bbe4a94bdff054a
Ralph Gabriel Mariano <57673613+KarateChap@users.noreply.github.com>
2022-08-26 18:15:34 +0800

    added NavBar and Hero page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Analytics from "./components/Analytics";
+import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import "./index.css";
 function App() {
   return (
     <>
       <NavBar />
+      <Hero />
+      <Analytics />
     </>
   );
 }
