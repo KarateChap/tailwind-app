8a8ee64b743f4db4831f2e728f02f1252e9fb12e
Ralph Gabriel Mariano <57673613+KarateChap@users.noreply.github.com>
2022-08-26 18:50:12 +0800

    added cards component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Analytics from "./components/Analytics";
+import Cards from "./components/Cards";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
+import Newsletter from "./components/Newsletter";
 import "./index.css";
+
 function App() {
   return (
     <>
       <NavBar />
       <Hero />
       <Analytics />
+      <Newsletter />
+      <Cards />
     </>
   );
 }
