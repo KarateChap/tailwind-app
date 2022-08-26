cd01d16056032c70f772d1f41afbb133dd8961e0
Ralph Gabriel Mariano <57673613+KarateChap@users.noreply.github.com>
2022-08-26 20:18:06 +0800

    test netlify

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,13 +11,13 @@ const NavBar = () => {
 
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-      <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
-      <ul className="hidden md:flex">
-        <li className="p-4">Home</li>
-        <li className="p-4">Company</li>
-        <li className="p-4">Resources</li>
-        <li className="p-4">About</li>
-        <li className="p-4">Contact</li>
+      <h1 className="w-full text-3xl font-bold text-[#00df9a] select-none">REACT. ELAMAE</h1>
+      <ul className="hidden md:flex select-none">
+        <li className="p-4 cursor-pointer hover:bg-[#00df9a] hover:rounded-md hover:text-[#000000]">Home</li>
+        <li className="p-4 cursor-pointer hover:bg-[#00df9a] hover:rounded-md hover:text-[#000000]">Company</li>
+        <li className="p-4 cursor-pointer hover:bg-[#00df9a] hover:rounded-md hover:text-[#000000]">Resources</li>
+        <li className="p-4 cursor-pointer hover:bg-[#00df9a] hover:rounded-md hover:text-[#000000]">About</li>
+        <li className="p-4 cursor-pointer hover:bg-[#00df9a] hover:rounded-md hover:text-[#000000]">Contact</li>
       </ul>
       <div onClick={handleNav} className="block md:hidden">
         {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
