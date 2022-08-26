f2cd7c23d3f3c94b83e187a11398baf74dbb74fc
Ralph Gabriel Mariano <57673613+KarateChap@users.noreply.github.com>
2022-08-26 20:19:22 +0800

    revert back

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
 
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-      <h1 className="w-full text-3xl font-bold text-[#00df9a] select-none">REACT. ELAMAE</h1>
+      <h1 className="w-full text-3xl font-bold text-[#00df9a] select-none">REACT.</h1>
       <ul className="hidden md:flex select-none">
         <li className="p-4 cursor-pointer hover:bg-[#00df9a] hover:rounded-md hover:text-[#000000]">Home</li>
         <li className="p-4 cursor-pointer hover:bg-[#00df9a] hover:rounded-md hover:text-[#000000]">Company</li>
